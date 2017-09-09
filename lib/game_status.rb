3313c75d6ec3aeef99f34adfc7ff842a627deb47
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-09 19:22:06 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [ 
+WIN_COMBINATIONS = [
 [0,1,2] #top rows
 [3,4,5] #middle rows
 [6,7,8] #bottom row
